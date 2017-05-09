fab4388783229cbd1f21b981e97a384d00c195c4
mugul <mugul@github.com>
2017-05-09 14:24:01 -0400

    probando bd

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-// var TelegramBot = require('node-telegram-bot-api');
 
-// var token = '389085214:AAGjB1U46UCeuAiUn09hqDpBUEA-UIERR0s';
-// // Setup polling way
-// var bot = new TelegramBot(token, {polling: true});
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
 
-// // Matches /echo [whatever]
-// bot.onText(/\/echo (.+)/, function (msg, match) {
-//   var fromId = msg.from.id;
-//   var resp = match[1];
-//   bot.sendMessage(fromId, resp);
-// });
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'mysql://mysql:3306/',
+    user     : 'user2HA',
+    password : 'eBR1gtKtw8VwD2Ad',
+    database : 'sampledb',
+    debug    :  false
+});
 
-// // Any kind of message
-// bot.on('message', function (msg) {
-//   var chatId = msg.chat.id;
-//   // photo can be: a file path, a stream or a Telegram file_id
-//   var photo = 'cats.png';
-//   bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
-// });
 
-// bot.onText(/\/help/, function(msg, match) {
-//   var fromId = msg.from.id;
-//   bot.sendMessage(fromId, "This spectacular bot just have one single command.\n/insult - Insult you.");
-// });
+
+
+
 
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -34,17 +27,41 @@ const token = '389085214:AAGjB1U46UCeuAiUn09hqDpBUEA-UIERR0s';
 const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
+
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
+
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
+
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
+  var resp = match[1]; // the captured "whatever"
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  
+  pool.getConnection(function(err,connection){
+    if (err) {
+      res.json({"code" : 100, "status" : "Error in connection database"});
+      return;
+      bot.sendMessage(chatId, "code 100, status: Error in connection database");
+    }   
+  
+
+
+  }
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
+
 
 // Listen for any kind of message. There are different kinds of
 // messages.
@@ -54,3 +71,7 @@ bot.on('message', (msg) => {
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
 });
+
+
+
+
